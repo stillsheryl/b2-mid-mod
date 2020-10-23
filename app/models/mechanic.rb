e2e9ece5cd804cf5d9973af559f109d5c9216eb2
Sheryl Stillman <sheryl.stillman@gmail.com>
2020-10-23 11:28:43 -0600

    Add functionality for button to add ride to list

--- app/models/mechanic.rb
+++ app/models/mechanic.rb
@@ -1,4 +1,9 @@
 class Mechanic < ApplicationRecord
   has_many :mechanic_rides
   has_many :rides, through: :mechanic_rides
+
+  def add_ride(ride_id)
+    id = ride_id.to_i
+    MechanicRide.create!(mechanic_id: self.id, ride_id: id)
+  end
 end
