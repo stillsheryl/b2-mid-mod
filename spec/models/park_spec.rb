9212fc877937924d156f3da500139ec160aff68a
Sheryl Stillman <sheryl.stillman@gmail.com>
2020-10-23 10:28:41 -0600

    Add test model method for average thrill rating

--- spec/models/park_spec.rb
+++ spec/models/park_spec.rb
@@ -4,4 +4,15 @@ describe Park, type: :model do
   describe "relationships" do
     it { should have_many :rides }
   end
+
+  describe "instance methods" do
+    it "average_thrill_rating" do
+      park = Park.create!(name: "Kali's Park", price: 50.00)
+      thunder_mountain = park.rides.create!(name: "Thunder Mountain", thrill_rating: 10)
+      merry = park.rides.create!(name: "Merry Go Round", thrill_rating: 2)
+      slide = park.rides.create!(name: "Giant Slide", thrill_rating: 6)
+
+      expect(park.average_thrill_rating).to eq(6)
+    end
+  end
 end
