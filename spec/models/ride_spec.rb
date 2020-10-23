1d2d5b5057b231e9d8c7d70e848e05767790cc47
Sheryl Stillman <sheryl.stillman@gmail.com>
2020-10-23 09:47:19 -0600

    Update test syntax

--- spec/models/ride_spec.rb
+++ spec/models/ride_spec.rb
@@ -2,6 +2,8 @@ require 'rails_helper'
 
 describe Ride, type: :model do
   describe "relationships" do
-    it { should belong_to :parks }
+    it { should belong_to :park }
+    it { should have_many :mechanic_rides }
+    it { should have_many(:mechanics).through(:mechanic_rides) }
   end
 end
